57fff625c097c3abeda53eb2386fe2c63fb2e8fc
VVNTHEFLASH <vishnurockhunters@gmail.com>
2021-08-28 20:13:01 +0530

    Project-12-superwars Stage-2 completed

--- src/app.js
+++ src/app.js
@@ -14,11 +14,11 @@ const PLAYERS = [
     "Hulk",
     "Ninja",
     "Black Cat",
-    "Volverine",
+    "Wolverine",
     "Thor",
     "Slayer",
     "Vader",
-    "Slingo"
+    "Thanos"
 ];
 
 // initialize players with image and strength
@@ -26,6 +26,15 @@ const initPlayers = (players) => {
     let detailedPlayers = [];
     // Create players using for loop
     // Type your code here
+    for(let i=0;i<players.length;i++) {
+        
+        detailedPlayers[i] = {
+            name: players[i],
+            strength: getRandomStrength(),
+            image:"images/super-"+(i+1)+".png",
+            type: (i%2 !=0) ? "villain":"hero"
+        }
+    }
 
     return detailedPlayers;
 }
@@ -34,6 +43,7 @@ const initPlayers = (players) => {
 const getRandomStrength = () => {
     // Return a random integer (0,100]
     // Note: You can use Math.random() and Math.ceil()
+    return Math.ceil(Math.random()*100);
 }
 
 const buildPlayers = (players, type) => {
@@ -42,6 +52,16 @@ const buildPlayers = (players, type) => {
     // Loop through players and accumulate HTML template
     // depending of type of player(hero|villain)
     // Type your code here
+    for(let i=0;i<players.length;i++) {
+
+        singlePlayer = `<div class="player">
+        <img src="${players[i].image}" alt="">
+        <div class="name">${players[i].name}</div>
+        <div class="strength">${players[i].strength}</div>
+    </div>`;
+    if(players[i].type == type) 
+        fragment += singlePlayer;
+    }
 
     return fragment;
 }
